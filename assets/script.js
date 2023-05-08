014e68e2dde9e047f86328f31f1c1cc7ab3024e3
j.crudenaire@laposte.net <j.crudenaire@laposte.net>
2023-05-08 17:32:22 +0200

    Carroussel next et preview ok

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,57 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "./assets/images/slideshow/slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "./assets/images/slideshow/slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "./assets/images/slideshow/slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "./assets/images/slideshow/slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrow_left = document.querySelector('.arrow_left');
+const arrow_right = document.querySelector('.arrow_right')
+
+let imageIndex = 0;
+let picture = document.querySelector('.banner-img');
+let banner = document.getElementById("banner");
+let paragraph = banner.querySelector("p");
+
+
+function carrousel() {
+	picture.src = slides[imageIndex].image;
+	paragraph.innerHTML = slides[imageIndex].tagLine;
+
+}
+
+arrow_left.addEventListener('click', function () {
+	console.log('ok');
+	imageIndex--
+	if (imageIndex <= 0) {
+		imageIndex = slides.length - 1;
+	}
+	carrousel();
+});
+arrow_right.addEventListener('click', function () {
+	console.log("ok");
+	imageIndex++
+	if (imageIndex >= slides.length) {
+		imageIndex = 0;
+	}
+	carrousel();
+});
+
+const parent = document.querySelector('.dots');
+for (let i = 0; i < slides.length; i += 1) {
+	const icone = document.createElement('div');
+	icone.classList.add('dot');
+	parent.appendChild(icone);
+};
