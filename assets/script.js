1141206e98652a35e4627d7e7cc5e8888c65d1e6
j.crudenaire@laposte.net <j.crudenaire@laposte.net>
2023-05-16 10:53:05 +0200

    projet5 sans animation

--- assets/script.js
+++ assets/script.js
@@ -26,6 +26,7 @@ let banner = document.getElementById("banner");
 let paragraph = banner.querySelector("p");
 let next = document.getElementById("slider");
 let previous = document.getElementById("slider_droite");
+let delay = 700;
 
 function carrousel() {
 	const parent = document.querySelector('.dots');
@@ -66,53 +67,25 @@ carrousel();
 
 arrow_left.addEventListener('click', function () {
 	console.log('ok');
-	const imageAnime = (imageIndex - 1 + slides.length) % slides.length;
 	imageIndex--;
 	if (imageIndex <= -1) {
 		imageIndex = slides.length - 1;
 	}
-	paragraph.innerHTML = slides[imageAnime].tagLine;
-	next.src = slides[imageAnime].image;
-	picture.classList.add('active');
-	next.classList.add('active', 'next')
-	next.classList.remove('next_anime')
-	picture.classList.add('previous');
-	picture.classList.remove('active')
-	setTimeout(() => {
-		picture.classList.add('active')
-		picture.classList.remove('previous')
-		next.classList.remove('active', 'next');
-		next.classList.add('next_anime');
-		picture.classList.remove('active');
-		picture.src = slides[imageIndex].image;
-	}, 800);
+	paragraph.innerHTML = slides[imageIndex].tagLine;
+
+	picture.src = slides[imageIndex].image;
 	dots();
 	carrousel();
-
 });
 arrow_right.addEventListener('click', function () {
 	console.log("ok");
-	const imageAnimeDroite = (imageIndex + 1 + slides.length) % slides.length;
 	imageIndex++;
 	if (imageIndex >= slides.length) {
 		imageIndex = 0;
 	}
 	paragraph.innerHTML = slides[imageIndex].tagLine;
-	previous.src = slides[imageAnimeDroite].image;
-	picture.classList.add('active');
-	previous.classList.add('active',);
-	previous.classList.remove('previous_anime')
-	picture.classList.add('next');
-	picture.classList.remove('active');
-	setTimeout(() => {
-		picture.classList.add('active');
-		picture.classList.remove('next');
-		previous.classList.remove('previous', 'active');
-		previous.classList.add('previous_anime')
-		picture.classList.remove('active');
-		picture.src = slides[imageIndex].image;
-	}, 800);
 
+	picture.src = slides[imageIndex].image;
 	dots();
 	carrousel();
 });
